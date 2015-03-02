079c692ed8d896c6bb9f8134fdb938ec7c501bb2
annrajala <annaleena.rajala@helsinki.fi>
2015-03-02 23:53:12 +0200

    Viikko 7

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -12,18 +12,26 @@ describe "New Beer page" do
   it "should be possible to create a new beer with a valid name" do
     visit new_beer_path
     fill_in('beer[name]', with:'Olut')
-
+    select('Lager', from:'beer[style_id]')
+    select('Koff', from:'beer[brewery_id]')
     expect{
       click_button "Create Beer"
     }.to change{Beer.count}.from(0).to(1)
   end
 
-  it "should not be possible to create a new beer with empty name" do
+  it "should not be possible to create a new beer with invalid name" do
+    sign_in({username: "Pekka", password:"Foobar1"})
     visit new_beer_path
-    click_button "Create Beer"
+    fill_in('beer[name]', with:'')
+    select('Lager', from:'beer[style_id]')
+    select('Koff', from:'beer[brewery_id]')
+    expect{
+      click_button "Create Beer"
+    }.not_to change{Beer.count}.from(0)
+    expect(brewery.beers.count).to eq(0)
     expect(page).to have_content "New beer"
     expect(page).to have_content "Name can't be blank"
-    expect(Beer.count).to eq(0)
+    expect(current_path).to eq(beers_path)
   end
 
 end
